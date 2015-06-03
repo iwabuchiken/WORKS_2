93d8b4e921b097554a643cf36921ddd7cae104db
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-06-03 22:31:31 +0900

    W

--- Utils/shortcuts/emb/start_command_prompot.bat
+++ Utils/shortcuts/emb/start_command_prompot.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-start C:\WORKS\Utils\shortcuts\emb\"Command Prompt.lnk"
+start C:\WORKS\Utils\shortcuts\emb\"Command Prompt.lnk" /k a
 
 exit
--- Utils/shortcuts/ode_first/start_command_prompot.bat
+++ Utils/shortcuts/ode_first/start_command_prompot.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-start C:\WORKS\Utils\shortcuts\ode_first\"Command Prompt.lnk"
+start C:\WORKS\Utils\shortcuts\ode_first\"Command Prompt.lnk" /k a
 
 
 exit
