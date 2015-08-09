7a931f0a559ace5188973482a711f7fe18816d51
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-09 23:18:06 +0900

    W

---
+++ Utils/shortcuts/cake_ig/a.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+C:\WORKS\a.bat
+
+
---
+++ Utils/shortcuts/cake_ig/start_git.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+pushd C:\WORKS\WS\Eclipse_Luna\Cake_NR5
+
+git config --global credential.helper wincred
+
+REM a
+
+REM pushd C:\WORKS\WS\Cake\Cake_NR5
+
+gitk
+
+pause
+
+exit
