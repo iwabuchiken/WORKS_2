0ed3015baa8b65a515603e4b12a2121fd2a61aae
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-09-10 02:00:50 +0900

    W

--- Utils/shortcuts/proj_MuseScore/start_apps.bat
+++ Utils/shortcuts/proj_MuseScore/start_apps.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
 
-start C:\WORKS\Programs\MuseScore_2.0.1\bin\MuseScore
+REM start C:\WORKS\Programs\MuseScore_2.0.1\bin\MuseScore
 
-"C:\WORKS\Programs\freeware\Free WAV to MP3 Converter\Free WAV to MP3 Converter.exe"
+pushd "C:\WORKS\Programs\freeware\Free MIDI to MP3 Converter"
+start MIDIToMP3.exe
+REM start "C:\WORKS\Programs\freeware\Free MIDI to MP3 Converter\MIDIToMP3.exe"
+REM "C:\WORKS\Programs\freeware\Free WAV to MP3 Converter\Free WAV to MP3 Converter.exe"
+
+
+start C:\WORKS\Programs\freeware\Rosegarden\rosegarden
