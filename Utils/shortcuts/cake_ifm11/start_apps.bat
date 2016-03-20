b3ccf637bfbed184c28594c83ff096176f3d466c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-03-20 23:46:54 +0900

    W

--- Utils/shortcuts/cake_ifm11/start_apps.bat
+++ Utils/shortcuts/cake_ifm11/start_apps.bat
@@ -10,5 +10,8 @@ start xampp-control.exe
 pushd C:\WORKS\Programs\FileZilla\FileZilla_FTP_Client
 start filezilla.exe
 
+REM SqliteBrowser
+pushd C:\WORKS\Programs\SqliteBrowser3\bin
+start sqlitebrowser
 
 REM exit
