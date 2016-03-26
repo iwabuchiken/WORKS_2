87e479eabb4bd27a90ea6583a5c668841ddd5d89
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-03-27 07:36:01 +0900

    W

--- Utils/commands/push_fm.bat
+++ Utils/commands/push_fm.bat
@@ -6,6 +6,8 @@ REM a p && cd C:\WORKS\WS\WS_Android~\FM && git add . && git commit -m "W" && gi
 REM echo cd C:\WORKS\WS\FM
 echo cd C:\WORKS\WS\FM_2
 REM C:\WORKS\a && cd C:\WORKS\WS\FM && git add . && git commit -m "W" && git push origin master
-C:\WORKS\a && cd C:\WORKS\WS\FM && git add -A && git commit -m "W" && git push origin master
+REM C:\WORKS\a && cd C:\WORKS\WS\FM && git add -A && git commit -m "W" && git push origin master
+C:\WORKS\a && cd C:\WORKS\WS\FM_2 && git add -A && git commit -m "W" && git push origin master
+
 
 pause
