d9029150c22a478181da0c61ac9e19c71937cf0d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-03-28 22:57:45 +0900

    W

---
+++ prep_from_mac.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo changing to C:\WORKS\WS\FM_2
+echo pushd C:\WORKS\WS\FM_2
+pushd C:\WORKS\WS\FM_2
+
+echo pulling from remote...
+echo g pull https://github.com/iwabuchiken/FM_2.git
+g pull https://github.com/iwabuchiken/FM_2.git
