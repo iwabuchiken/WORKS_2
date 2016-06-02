e4333f1557103faad4147a08f9f9191407ffb5b3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-06-02 21:10:44 +0900

    W

--- Utils/shortcuts/proj_Music/start_apps.bat
+++ Utils/shortcuts/proj_Music/start_apps.bat
@@ -4,3 +4,11 @@ echo starting C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
 
 start C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
 
+pushd C:\WORKS\Programs\FileZilla\FileZilla_FTP_Client
+start filezilla.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
+start chrome.exe http://www.shamisen.info/chogen.koto1.09/newpage3.html#nogi
+
+REM start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
