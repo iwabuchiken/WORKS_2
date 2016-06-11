cd0c7665d4d18a06d90453bb8c856c215db96999
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-06-11 22:46:24 +0900

    W

--- Utils/shortcuts/proj_Music/start_apps.bat
+++ Utils/shortcuts/proj_Music/start_apps.bat
@@ -11,5 +11,7 @@ pushd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
 start chrome.exe http://www.shamisen.info/chogen.koto1.09/newpage3.html#nogi
 start chrome.exe https://en.wikipedia.org/wiki/File:Circle_of_fifths_deluxe_4.svg
+start chrome.exe https://mysqladmin.lolipop.jp/pma/index.php?db=LAA0278957-cakeifm11&server=113&token=d111622579c32be7d61e8cf9219daee5
+start chrome.exe https://docs.google.com/spreadsheets/d/1GlMjFYCAdSc87V-BhGAM-sz-Kka6AlyxgS-0jqvPlPc/edit#gid=0
 
 REM start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
