d3773df0925b726615f3be621af4267547d52eba
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-06-15 21:57:38 +0900

    W

--- Utils/shortcuts/proj_Music/start_apps.bat
+++ Utils/shortcuts/proj_Music/start_apps.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 
-echo starting C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
+REM echo starting C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
 
-start C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
+REM start C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
 
-pushd C:\WORKS\Programs\FileZilla\FileZilla_FTP_Client
-start filezilla.exe
+REM pushd C:\WORKS\Programs\FileZilla\FileZilla_FTP_Client
+REM start filezilla.exe
 
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
@@ -13,5 +13,6 @@ start chrome.exe http://www.shamisen.info/chogen.koto1.09/newpage3.html#nogi
 start chrome.exe https://en.wikipedia.org/wiki/File:Circle_of_fifths_deluxe_4.svg
 start chrome.exe https://mysqladmin.lolipop.jp/pma/index.php?db=LAA0278957-cakeifm11&server=113&token=d111622579c32be7d61e8cf9219daee5
 start chrome.exe https://docs.google.com/spreadsheets/d/1GlMjFYCAdSc87V-BhGAM-sz-Kka6AlyxgS-0jqvPlPc/edit#gid=0
+start chrome.exe https://mail.google.com/mail/u/0/?shva=1#inbox
 
 REM start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
