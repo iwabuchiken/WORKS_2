af19e35f249af182ca0b9edab6019e5cfcffdfd8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-11-09 22:05:36 +0900

    W

--- Utils/shortcuts/proj_Music/start_apps.bat
+++ Utils/shortcuts/proj_Music/start_apps.bat
@@ -8,14 +8,14 @@ REM pushd C:\WORKS\Programs\FileZilla\FileZilla_FTP_Client
 REM start filezilla.exe
 
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
-start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
-start chrome.exe http://www.shamisen.info/chogen.koto1.09/newpage3.html#nogi
-start chrome.exe https://en.wikipedia.org/wiki/File:Circle_of_fifths_deluxe_4.svg
-start chrome.exe https://mysqladmin.lolipop.jp/pma/index.php?db=LAA0278957-cakeifm11&server=113&token=d111622579c32be7d61e8cf9219daee5
-start chrome.exe https://docs.google.com/spreadsheets/d/1GlMjFYCAdSc87V-BhGAM-sz-Kka6AlyxgS-0jqvPlPc/edit#gid=0
+REM start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
+REM start chrome.exe http://www.shamisen.info/chogen.koto1.09/newpage3.html#nogi
+REM start chrome.exe https://en.wikipedia.org/wiki/File:Circle_of_fifths_deluxe_4.svg
+REM start chrome.exe https://mysqladmin.lolipop.jp/pma/index.php?db=LAA0278957-cakeifm11&server=113&token=d111622579c32be7d61e8cf9219daee5
+REM start chrome.exe https://docs.google.com/spreadsheets/d/1GlMjFYCAdSc87V-BhGAM-sz-Kka6AlyxgS-0jqvPlPc/edit#gid=0
 start chrome.exe https://mail.google.com/mail/u/0/?shva=1#inbox
 start chrome.exe http://www.ofoct.com/audio-converter/convert-midi-to-mp3-or-wav-ogg-aac-wma.html
-start chrome.exe http://media.io/
+REM start chrome.exe http://media.io/
 
 
 REM start C:\WORKS\Programs\freeware\SplitPhotos\SplitPhotos
