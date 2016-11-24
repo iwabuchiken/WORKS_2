101a76881a00757459efa5012632858458b89826
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-11-25 01:03:02 +0900

    W

--- Utils/shortcuts/proj_FX/start_apps.bat
+++ Utils/shortcuts/proj_FX/start_apps.bat
@@ -15,6 +15,7 @@ REM start chrome.exe "https://docs.google.com/spreadsheets/d/1z5Rq680qTlL4SZ6GMQ
 start chrome.exe "https://docs.google.com/spreadsheets/d/1n5XLZ3ns0lInsYgAodVMW6woAEtYumSCVE8qSVUXIeA/edit#gid=671977138"
 
 start C:\Users\iwabuchiken\AppData\Roaming\MetaQuotes\Terminal\34B08C83A5AAE27A4079DE708E60511E\MQL4
+start C:\WORKS_2\WS\WS_FX
 
 pushd C:\WORKS_2\Utils\shortcuts\proj_FX
 start start_command_prompot.bat
