4d46045dcee205243656721c1881803067b651b2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-12-15 14:17:31 +0900

    W

--- Utils/shortcuts/proj_FX/main.bat
+++ Utils/shortcuts/proj_FX/main.bat
@@ -4,7 +4,7 @@ pushd C:\WORKS_2\Utils\shortcuts\proj_FX
 
 REM start start_git
 
-start start_apps
+REM start start_apps
 
 call start_command_prompot.bat
 REM call start_command_prompot.bat "a && clisp"
@@ -14,5 +14,7 @@ REM open_folder
 start start_git
 
 REM open_folder
+
+start start_apps
 
 exit
