77fabd1af0c6dc1dd197d75060e329ea872ded5e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-02-14 21:50:14 +0900

    W

---
+++ Utils/shortcuts/proj_home-appliances-engineer/main.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+REM pushd C:\WORKS\Utils\shortcuts\proj_Art
+pushd C:\WORKS_2\Utils\shortcuts\proj_home-appliances-engineer
+
+REM start C:\WORKS\Utils\shortcuts\and_cm7
+
+REM start start_git
+
+REM start start_command_prompot.bat
+
+REM start start_git
+
+start start_apps
+REM start start_applications.pl
+
+REM "call" is necessary; otherwise, start_git won't be executed
+REm call start_command_prompot.bat
+REM call start_command_prompot.bat "a && clisp"
+
+REM open_folder
+
+REM start start_git
+
+REM open_folder
+
+exit
