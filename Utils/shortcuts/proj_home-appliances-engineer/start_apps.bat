e1a43d9b9260e6b5f0387dac3a1b81b6f72a30d7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-02-20 23:33:27 +0900

    W

--- Utils/shortcuts/proj_home-appliances-engineer/start_apps.bat
+++ Utils/shortcuts/proj_home-appliances-engineer/start_apps.bat
@@ -4,7 +4,7 @@ REM ------------------------ player
 start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
 
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
-start chrome.exe https://mail.google.com/mail/u/0/?shva=1#search/%%E5%%AE%%B6%%E9%%9B%%BB+myself/15a366364e7d8fa6
+start chrome.exe https://mail.google.com/mail/u/0/?shva=1#search/%%E5%%AE%%B6%%E9%%9B%%BB+myself?compose=15a556deef3acd26
 start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=%%E5%%AE%%B6%%E9%%9B%%BB%%E3%%82%%A8%%E3%%83%%B3%%E3%%82%%B8%%E3%%83%%8B%%E3%%82%%A2&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 start chrome.exe "https://ja.wikipedia.org/wiki/%%E3%%83%%A1%%E3%%82%%A4%%E3%%83%%B3%%E3%%83%%9A%%E3%%83%%BC%%E3%%82%%B8"
 start chrome.exe https://translate.google.co.jp/?hl=ja
---
+++ Utils/shortcuts/proj_sotec-review-papers/main.stop.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+REM taskkill /f /IM eclipse.exe
+taskkill /f /IM WScilex.exe
+taskkill /f /IM wish.exe
+
+
+exit
