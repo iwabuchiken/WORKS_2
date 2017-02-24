7e04382aad28f1930f8b1fce3b501eda5002ebce
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-02-25 00:06:03 +0900

    W

---
+++ Utils/shortcuts/proj_electrical-worker.2/start_apps.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM ------------------------ player
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe https://mail.google.com/mail/u/0/?shva=1#search/%%E9%%9B%%BB%%E6%%B0%%97%%E5%%B7%%A5%%E4%%BA%%8B%%E5%%A3%%AB%%EF%%BC%%92%%E7%%A8%%AE+myself
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=%%E9%%9B%%BB%%E6%%B0%%97%%E5%%B7%%A5%%E4%%BA%%8B%%E5%%A3%%AB%%EF%%BC%%92%%E7%%A8%%AE&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://ja.wikipedia.org/wiki/%%E3%%83%%A1%%E3%%82%%A4%%E3%%83%%B3%%E3%%83%%9A%%E3%%83%%BC%%E3%%82%%B8"
+start chrome.exe https://translate.google.co.jp/?hl=ja
+start chrome.exe http://tech-unlimited.com/urlencode.html
+REM start "chrome.exe https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8"
+
+REM "%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" /prefetch:1
+
+
+
+REM pushd C:\WORKS\Programs\freeware\Blender Foundation\Blender_2.74
+REM start blender
+
+exit
