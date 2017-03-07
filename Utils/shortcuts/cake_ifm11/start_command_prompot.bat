2bb7481476620b23c3836e382b7613100971e549
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-07 23:13:08 +0900

    W

--- Utils/shortcuts/cake_ifm11/start_command_prompot.bat
+++ Utils/shortcuts/cake_ifm11/start_command_prompot.bat
@@ -1,6 +1,16 @@
 @ECHO OFF
 
-start C:\WORKS_2\Utils\shortcuts\cake_ifm11\"Command Prompt.lnk" /k a
+rem start C:\WORKS_2\Utils\shortcuts\cake_ifm11\"Command Prompt.lnk" /k a
+rem start C:\WORKS_2\Utils\shortcuts\cake_ifm11\"Command Prompt" /k a
+rem start "C:\WORKS_2\Utils\shortcuts\cake_ifm11\"Command Prompt\" /k a"
+rem start "C:\WORKS_2\Utils\shortcuts\cake_ifm11^"Command Prompt^	" /k a"
+rem start C:\WORKS_2\Utils\shortcuts\cake_ifm11\"Command Prompt" /k a
 
+%windir%\system32\cmd.exe C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11
+rem %windir%\system32\cmd.exe
 
-exit
+pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11
+
+a
+
+rem exit
---
+++ Utils/shortcuts/proj_hazardous-materials-otsu-4/start_apps.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM ------------------------ player
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe https://mail.google.com/mail/u/0/?shva=1#search/%%E5%%8D%%B1%%E9%%99%%BA%%E7%%89%%A9%%E3%%80%%80%%E4%%B9%%99%%EF%%BC%%94+myself?compose=15aa7d19e8aed9ec
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=%%E5%%8D%%B1%%E9%%99%%BA%%E7%%89%%A9+%%E4%%B9%%99%%EF%%BC%%94&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://ja.wikipedia.org/wiki/%%E3%%83%%A1%%E3%%82%%A4%%E3%%83%%B3%%E3%%83%%9A%%E3%%83%%BC%%E3%%82%%B8"
+start chrome.exe https://translate.google.co.jp/?hl=ja
+start chrome.exe http://tech-unlimited.com/urlencode.html
+REM start "chrome.exe https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8"
+
+REM "%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe" /prefetch:1
+
+
+
+REM pushd C:\WORKS\Programs\freeware\Blender Foundation\Blender_2.74
+REM start blender
+
+exit
