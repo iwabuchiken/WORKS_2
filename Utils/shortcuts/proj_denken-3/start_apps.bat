3252ed454b889b34f17364425775a96ca57dcb3a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-17 00:48:07 +0900

    W

---
+++ Utils/shortcuts/proj_denken-3/start_apps.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+REM ------------------------ player
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/ìdå±ÇR+myself"
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=ìdå±ÇR&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://ja.wikipedia.org/wiki/ìdå±"
+start chrome.exe https://translate.google.co.jp/?hl=ja
+start chrome.exe http://tech-unlimited.com/urlencode.html
+
+exit
