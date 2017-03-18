46b96406c9480a2dffadfa6e13c7f982119b70b6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-19 01:45:26 +0900

    W

--- Utils/shortcuts/proj_hebrew/start_apps.bat
+++ Utils/shortcuts/proj_hebrew/start_apps.bat
@@ -4,6 +4,8 @@ pushd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/ヘブライ語+myself+-daylog?compose=15a556deef3acd26"
 start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=ヘブライ語&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 start chrome.exe "https://ja.wikipedia.org/wiki/ヘブライ語"
+start chrome.exe "http://qbible.com/hebrew-old-testament/proverbs/1.html#1"
+start chrome.exe http://biblehub.com/text/proverbs/1-5.htm
 start chrome.exe https://translate.google.co.jp/?hl=ja
 start chrome.exe http://tech-unlimited.com/urlencode.html
 
