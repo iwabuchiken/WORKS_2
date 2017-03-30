5ec5ab947dca44c9ee178e45781348dcf97b8c52
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-31 00:19:07 +0900

    W

--- Utils/shortcuts/proj_denken-3/start_apps.bat
+++ Utils/shortcuts/proj_denken-3/start_apps.bat
@@ -10,6 +10,7 @@ start chrome.exe "https://ja.wikipedia.org/wiki/ìdå±"
 start chrome.exe https://translate.google.co.jp/?hl=ja
 start chrome.exe http://tech-unlimited.com/urlencode.html
 start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+start chrome.exe https://www.youtube.com/
 
 REM ===============================
 REM 	open folder
