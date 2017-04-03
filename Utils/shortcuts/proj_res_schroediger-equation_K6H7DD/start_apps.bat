35e33c76d4f3dc06a4479df6524d7e0fbab3ac51
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-04-04 01:54:44 +0900

    W

---
+++ Utils/shortcuts/proj_res_schroediger-equation_K6H7DD/start_apps.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/K6H7DD+myself+-daylog?compose=15a556deef3acd26"
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=K6H7DD&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://ja.wikipedia.org/wiki/シュレーディンガー方程式"
+start chrome.exe "https://www.youtube.com/results?search_query=particle+physics"
+start chrome.exe https://translate.google.co.jp/?hl=ja
+start chrome.exe http://tech-unlimited.com/urlencode.html
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+exit
