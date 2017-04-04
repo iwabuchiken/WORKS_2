d39cf62b0eea96fedd119633b7bcb5c315a66c39
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-04-05 00:53:12 +0900

    W

---
+++ Utils/shortcuts/proj_res_maxima_R22MPV/start_apps.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/R22MPV+myself+-daylog?compose=15a556deef3acd26"
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=R22MPV&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://ja.wikipedia.org/wiki/Maxima"
+start chrome.exe "https://www.youtube.com/results?search_query=maxima+algebra"
+start chrome.exe https://translate.google.co.jp/?hl=ja
+start chrome.exe http://tech-unlimited.com/urlencode.html
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+
+REM ===============================
+REM 	open folder
+REM ===============================
+rem start C:\WORKS_2\WS\WS_Others
+start C:\WORKS_2\WS\WS_Others\free#\R22MPV
+
+REM ===============================
+REM 	git
+REM ===============================
+pushd C:\WORKS_2\Utils\shortcuts\proj_denken-3
+rem pushd C:\WORKS_2\WS\WS_Others
+start_git.bat
+
+
+exit
