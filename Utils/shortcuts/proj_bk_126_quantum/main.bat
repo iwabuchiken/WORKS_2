80597792a12f0ba65991c59315d7d3f095aac0ad
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-04-25 01:15:59 +0900

    W

---
+++ Utils/shortcuts/proj_bk_126_quantum/main.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+pushd C:\WORKS_2\Utils\shortcuts\proj_bk_126_quantum
+
+start_apps.bat
+
+exit
---
+++ Utils/shortcuts/proj_bk_126_quantum/start_apps.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/myself+bk-126+-daylog?compose=15a556deef3acd26"
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=bk-126&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://ja.wikipedia.org/wiki/量子コンピュータ"
+start chrome.exe "https://www.youtube.com/results?search_query=quantum+computer"
+start chrome.exe https://translate.google.co.jp/?hl=ja
+start chrome.exe http://tech-unlimited.com/urlencode.html
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+
+rem pushd "C:\WORKS_2\Programs\Mozilla Firefox"
+rem start firefox.exe "https://mail.google.com/mail/u/0/?shva=1#search/4JZVMD+myself+-daylog?compose=15a556deef3acd26"
+rem start firefox.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=4JZVMD&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+rem start firefox.exe "https://ja.wikipedia.org/wiki/素粒子"
+rem start firefox.exe "https://www.youtube.com/results?search_query=particle+physics"
+rem start firefox.exe https://translate.google.co.jp/?hl=ja
+rem start firefox.exe http://tech-unlimited.com/urlencode.html
+rem start firefox.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+
+
+exit
+
