3ca582d8d677b9d827e251a349a645c4f44e8cd2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-04-28 01:24:05 +0900

    W

--- Utils/shortcuts/prog_C_SoundProg/main.bat
+++ Utils/shortcuts/prog_C_SoundProg/main.bat
@@ -15,7 +15,8 @@ REM start_git_c
 
 start open_folder
 
-pushd C:\WORKS_2\Programs\eclipse\cpp-neon2\eclipse
+rem pushd C:\WORKS_2\Programs\eclipse\cpp-neon2\eclipse
+pushd C:\WORKS_2\Programs\eclipse\java-neon_2\eclipse
 eclipse
 
 
