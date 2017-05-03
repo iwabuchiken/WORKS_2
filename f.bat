2ae155b0e6f709b2744f2ba93656a40c2d50b57f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-05-04 00:09:26 +0900

    W

---
+++ f.bat
@@ -0,0 +1,6 @@
+rem start C:\WORKS_2\WS\WS_Music
+
+rem ref http://stackoverflow.com/questions/607670/windows-shell-command-to-get-the-full-path-to-current-directory answered Mar 3 '09 at 19:05
+start %cd%
+
+rem echo %cd%
