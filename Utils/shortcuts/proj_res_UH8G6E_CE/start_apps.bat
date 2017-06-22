54c3ca73cd5ae248d20bde974a93b619f204daea
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-22 23:27:44 +0900

    W

--- Utils/shortcuts/proj_res_UH8G6E_CE/start_apps.bat
+++ Utils/shortcuts/proj_res_UH8G6E_CE/start_apps.bat
@@ -13,10 +13,11 @@ rem 	browser
 rem 
 rem ==========================
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
-start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/myself+-daylog+VX7GLZ+NLP"
-start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=VX7GLZ+NLP&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/myself+-daylog+UH8G6E"
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=UH8G6E&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
-start chrome.exe http://localhost/Eclipse_Luna/Cake_NR5/pieces/
+start chrome.exe http://localhost/WS_Others/free/UH8G6E_CE/
+
 
 rem ==========================
 rem 	Eclipse
