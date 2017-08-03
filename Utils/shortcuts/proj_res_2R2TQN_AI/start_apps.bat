d89fef5ec8c66c4c93028a6b41f55518fd88ab5c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-08-03 23:06:29 +0900

    W

---
+++ Utils/shortcuts/proj_res_2R2TQN_AI/start_apps.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+
+rem ==========================
+rem 
+rem 	maxima
+rem 
+rem ==========================
+start C:\WORKS_2\Programs\Maxima-sbcl-5.38.1\wxMaxima\wxmaxima.exe
+
+rem ==========================
+rem 
+rem 	browser
+rem 
+rem ==========================
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe "https://mail.google.com/mail/u/0/?shva=1#search/myself+-daylog+2R2TQN"
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=2R2TQN&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+start chrome.exe http://localhost/WS_Others/free/2R2TQN_AI/
+
+
+rem ==========================
+rem 	Eclipse
+rem ==========================
+pushd C:\WORKS_2\Programs\eclipse\java-neon_2\eclipse
+
+start eclipse
+
+rem ==========================
+rem 
+rem 	folder
+rem 
+rem ==========================
+pushd C:\WORKS_2\Utils\shortcuts\proj_res_2R2TQN_AI
+start open_folder
+
+rem ==========================
+rem 
+rem 	xampp
+rem 	ref : C:\WORKS_2\Utils\shortcuts\proj_res_science_VX7GLZ__NLP\start_apps_xampp.bat
+rem 
+rem ==========================
+start C:\WORKS_2\WS\WS_Others\works_2\start_xampp.rb
+
+
+rem ==========================
+rem 
+rem 	git
+rem 
+rem ==========================
+pushd C:\WORKS_2\Utils\shortcuts\proj_res_2R2TQN_AI
+start start_git
+
