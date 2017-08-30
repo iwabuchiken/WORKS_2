10b1ec1c66f7648b7b45cf841de930ceeb222771
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-08-30 23:14:22 +0900

    W

---
+++ Utils/shortcuts/proj_dessin/start_apps.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe https://mail.google.com/mail/u/0/?shva=1#search/myself+d-17
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=des.&RBs_AND_OR_Memo=AND&sort=file_name&direction=desc"
+start chrome.exe "http://benfranklin.chips.jp/WS/WS_Others/free/UH8G6E_CE/1_X/1_14/main.(1.14~).php?url="
+
+exit
