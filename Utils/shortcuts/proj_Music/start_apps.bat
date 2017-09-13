4b61ef881b03aa3051e4820a35ada26c9790a97d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-09-13 23:06:46 +0900

    W

--- Utils/shortcuts/proj_Music/start_apps.bat
+++ Utils/shortcuts/proj_Music/start_apps.bat
@@ -7,7 +7,17 @@ REM start C:\WORKS\Programs\MuyseScore\MuseScore_2.3.1\bin\MuseScore
 REM pushd C:\WORKS\Programs\FileZilla\FileZilla_FTP_Client
 REM start filezilla.exe
 
-rem pushd "C:\Program Files (x86)\Google\Chrome\Application"
+REM **************************************
+REM 	Web pages
+REM **************************************
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe https://mail.google.com/mail/u/0/?shva=1#search/music+d-3?compose=15e7a1cc90226e61
+
+start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=music+clarinet+%%E6%%A5%%BD%%E8%%AD%%9C%%E3%%83%%A1%%E3%%83%%A2&RBs_AND_OR_Memo=AND&sort=file_name&direction=desc"
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/21495
+
+start chrome.exe http://www.zamzar.com/
+
 REM start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
 REM start chrome.exe http://www.shamisen.info/chogen.koto1.09/newpage3.html#nogi
 REM start chrome.exe https://en.wikipedia.org/wiki/File:Circle_of_fifths_deluxe_4.svg
@@ -20,14 +30,22 @@ REM start chrome.exe http://www.ofoct.com/audio-converter/convert-midi-to-mp3-or
 
 rem start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=music+%3Am&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 
-rem start chrome.exe http://www.zamzar.com/
-
 REM start chrome.exe http://media.io/
 
 rem start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
 
+REM **************************************
+REM 	Apps
+REM **************************************
 pushd C:\WORKS_2\Programs\Domino143
 start Domino
+
+pushd C:\WORKS_2\Programs\MuseScore 2\bin
+start MuseScore
+
+pause
+
+exit
 
 REM start C:\WORKS\Programs\freeware\SplitPhotos\SplitPhotos
 
