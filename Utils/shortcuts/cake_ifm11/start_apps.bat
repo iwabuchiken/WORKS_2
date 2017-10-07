2a6c31c5ceac69c785491ca6f2586a5179220a41
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-10-08 00:11:57 +0900

    W

--- Utils/shortcuts/cake_ifm11/start_apps.bat
+++ Utils/shortcuts/cake_ifm11/start_apps.bat
@@ -1,43 +1,18 @@
 @ECHO OFF
-
 REM ========================= OpenOffice
-REM start "C:\Program Files (x86)\OpenOffice 4\program\soffice.exe"
 
-start "C:\WORKS_2\Programs\OpenOffice 4\program\soffice.exe"
-
-
-REM pushd C:\WORKS_2\Programs\eclipse_luna
-REM pushd C:\WORKS_2\Programs\eclipse\java-neon\eclipse
+pushd "C:\WORKS_2\Programs\OpenOffice 4\program"
+start soffice.exe
+REM start "C:\WORKS_2\Programs\OpenOffice 4\program\soffice.exe"
 
 pushd C:\WORKS_2\Programs\eclipse\eclipse_luna-php
 start eclipse.exe
 
-
-REM pushd C:\WORKS_2\Programs\xampp
-
 pushd C:\xampp_5.6
 start xampp-control.exe
 
-REM pushd C:\WORKS_2\Programs\FileZilla\FileZilla_FTP_Client
-
-pushd C:\WORKS_2\Programs\FileZilla FTP Client
+pushd "C:\WORKS_2\Programs\FileZilla FTP Client"
 start filezilla.exe
-
-REM SqliteBrowser
-rem pushd C:\WORKS_2\Programs\SqliteBrowser3\bin
-rem start sqlitebrowser
-
-rem start "C:\Program Files\DB Browser for SQLite\3.9.0\bin\DB Browser for SQLite"
-REM "C:\Program Files\DB Browser for SQLite\3.9.0\bin\DB Browser for SQLite"
-REM "C:\WORKS_2\Programs\DB Browser for SQLite\DB Browser for SQLite"
-rem start "C:\WORKS_2\Programs\DB Browser for SQLite\DB Browser for SQLite"
-
-REM "C:\WORKS_2\Programs\DB Browser for SQLite\DB Browser for SQLite"
 "C:\WORKS_2\Programs\DB Browser for SQLite.3.10.0\DB Browser for SQLite"
-
-REM C:\WORKS_2\Programs\DB Browser for SQLite
-
-
-
 
 exit
