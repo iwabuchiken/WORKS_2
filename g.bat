eb693b8731170a643b1636ee227658f4d0cafd3d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-12-12 22:58:34 +0900

    W

---
+++ g.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM start "C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe"
+
+REM C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe %1 %2
+C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe %1 %2
+
--- gitk.bat
+++ gitk.bat
@@ -1,6 +1,8 @@
 @echo off
 
-start "C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe"
+REM start "C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe"
+
+start C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe
 
 REM "C:\Program Files\Git\cmd\gitk" %1 %2
 
