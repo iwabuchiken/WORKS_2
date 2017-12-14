e345caa99ab7afb2f45f0020db172302aab133ad
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-12-15 07:37:20 +0900

    edit files
    ---
    modified:   t.bat
    modified:   t.rb

--- t.bat
+++ t.bat
@@ -1,5 +1,28 @@
 @echo off
 
+if "%1"=="b2" (
+
+	C:\WORKS_2\Programs\Ruby23-x64\bin\ruby.exe C:\WORKS_2\t.rb %1 %2 %3
+	
+REM 	echo.
+	
+REM 	echo goint to end...
+	
+	goto end
+	
+) else if "%1"=="c2" (
+
+	C:\WORKS_2\Programs\Ruby23-x64\bin\ruby.exe C:\WORKS_2\t.rb %1 %2 %3
+	C:\WORKS_2\Programs\Ruby23-x64\bin\ruby.exe C:\WORKS_2\t.rb %1 %2 %3 | clip
+	
+	echo.
+	
+REM 	echo goint to end...
+	
+	goto end
+	
+)
+
 echo param is '%1'
 
 REM ruby C:\WORKS_2\t.rb
@@ -14,4 +37,5 @@ REM ref http://stackoverflow.com/questions/132799/how-can-you-echo-a-newline-in-
 echo.
 echo done
 
+:end
 
