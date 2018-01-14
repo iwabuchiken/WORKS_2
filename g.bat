cc2118b97669e3f42edc27eb4fcdcef7b887bc43
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-01-14 23:33:48 +0900

    W

--- g.bat
+++ g.bat
@@ -3,5 +3,8 @@
 REM start "C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe"
 
 REM C:\WORKS_2\Programs\Git_2.14.1\cmd\gitk.exe %1 %2
-C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe %1 %2
+REM C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe %1 %2
+
+REM new : 2018/01/11 12:58:55
+C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe %1 %2 %3
 
--- start_2.bat
+++ start_2.bat
@@ -6,7 +6,7 @@ pushd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome
 
 pushd "C:\Program Files (x86)\Mozilla Thunderbird"
-thunderbird
+start thunderbird
 
 start C:\WORKS_2
 
