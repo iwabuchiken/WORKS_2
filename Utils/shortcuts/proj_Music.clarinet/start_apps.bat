0b0e046b8d8fc252f191dcce89ba22bff7921ee9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-04-08 22:02:50 +0900

    W

--- Utils/shortcuts/proj_Music.clarinet/start_apps.bat
+++ Utils/shortcuts/proj_Music.clarinet/start_apps.bat
@@ -6,8 +6,11 @@ REM #############################
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome.exe https://www.onlinepianist.com/virtual-piano
 start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=clari&RBs_AND_OR_Memo=AND&sort=file_name&direction=desc"
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/24777
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_file_name=2018-04-07_17-21-08_000&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND
 start chrome.exe http://www.maccorp.co.jp/jm/unshi/clarinet.pdf
 start chrome.exe https://www.flashcardmachine.com/birt-clarinet-keynames.html
+
 
 REM #############################
 REM 	folder
