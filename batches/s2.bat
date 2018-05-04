0d321efa6e66d94b7b25b8c17a70da6b916b560d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-05-04 14:07:29 +0900

    Updates
    ---
    g2 c -a
    
    # Changes to be committed:
    #	new file:   MQL4_Indicators-EAs
    #	modified:   batches/s2.bat

--- batches/s2.bat
+++ batches/s2.bat
@@ -7,8 +7,12 @@ REM pushd "C:\Program Files\Git\cmd"
 REM start gitk
 
 REM start gitk2
-start C:\WORKS_2\gitk2
+REM start C:\WORKS_2\gitk2
+REM gitk2
+REM start C:\WORKS_2\batches\s2.bat
 
 rEM "C:\Program Files\Git\cmd\gitk"
 
 REM exit
+
+start C:\WORKS_2\batches\gitk2.bat
