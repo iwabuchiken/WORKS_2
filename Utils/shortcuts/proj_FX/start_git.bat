41b7631d2eddf181c5a12fdb16b9ce162e6cff08
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-10-02 22:22:37 +0900

    W

--- Utils/shortcuts/proj_FX/start_git.bat
+++ Utils/shortcuts/proj_FX/start_git.bat
@@ -1,19 +1,7 @@
-@ECHO OFF
-pushd C:\Users\iwabuchiken\AppData\Roaming\MetaQuotes\Terminal\34B08C83A5AAE27A4079DE708E60511E\MQL4
+REM @ECHO OFF
 
-call a.bat
+C:\WORKS_2\batches\r.bat o1 && C:\WORKS_2\batches\s.bat
 
-REM C:\WORKS_2\Programs\Git\cmd\gitk
-REM gitk
+pause
 
-REM "start" added => the console window still stays
-REM start gitk
-
-REM gitk
-
-REM C:\WORKS_2\Programs\Git\cmd\gitk
-pushd C:\Users\iwabuchiken\AppData\Roaming\MetaQuotes\Terminal\34B08C83A5AAE27A4079DE708E60511E\MQL4 && gitk
-
-REM pause 
-
-exit
+REM exit
---
+++ Utils/shortcuts/proj_Music.kb.listing/main.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+pushd C:\WORKS_2\Utils\shortcuts\proj_Music.kb.listing
+
+REM start C:\WORKS_2\Utils\shortcuts\and_cm7
+
+REM start start_git
+
+rem start start_git
+
+start start_apps
+REM start start_applications.pl
+
+REM "call" is necessary; otherwise, start_git won't be executed
+call start_command_prompot.bat
+
+exit
