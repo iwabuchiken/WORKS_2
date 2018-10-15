23d866db48e07f610246eb4c084d4e6f049cf5ae
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-10-15 23:22:09 +0900

    W

---
+++ Utils/shortcuts/proj_Art.animation/end_apps.bat
@@ -0,0 +1,8 @@
+taskkill /im wish.exe
+taskkill /im gimp-2.8.exe
+REM taskkill /im soffice.exe
+taskkill /im pencil2d.exe
+
+taskkill /im vlc.exe
+
+pause
---
+++ Utils/shortcuts/proj_Art.animation/start_ip-server.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+:execute
+
+start C:\WORKS_2\Utils\shortcuts\proj_Art.animation\"Command Prompt.lnk" /k "a && r w && r dd"
+
+REM start C:\WORKS_2\Utils\shortcuts\proj_Art\"Command Prompt.lnk" /k %arg%
+REM start C:\WORKS\Utils\shortcuts\prog_Lisp\"Command Prompt.lnk" /k "a && dir"
+
+
+:end
+
+pause
+
