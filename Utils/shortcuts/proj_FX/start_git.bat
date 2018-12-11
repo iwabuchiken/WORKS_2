e7edc040556e537165ea0343582d5c8180d27146
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-12-11 23:36:22 +0900

    W

--- Utils/shortcuts/proj_FX/start_git.bat
+++ Utils/shortcuts/proj_FX/start_git.bat
@@ -4,7 +4,7 @@ REM **********************************************
 REM 	git for : C:\WORKS_2\WS\WS_Others.prog
 REM **********************************************
 REM C:\WORKS_2\batches\r.bat o1 && C:\WORKS_2\batches\s.bat
-pushd C:\WORKS_2\batches\r.bat o1
+C:\WORKS_2\batches\r.bat o1
 call C:\WORKS_2\batches\s.bat
 
 REM **********************************************
