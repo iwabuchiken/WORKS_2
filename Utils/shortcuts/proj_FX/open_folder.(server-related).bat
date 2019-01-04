43e7b0be7ddb8dcd65348c32b5f19eb0815b1856
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-01-04 22:49:44 +0900

    W

---
+++ Utils/shortcuts/proj_FX/open_folder.(server-related).bat
@@ -0,0 +1,8 @@
+REM *************************************
+REM 	folders
+REM *************************************
+start C:\WORKS_2\WS\WS_Others.prog\prog\D-7\2_2\VIRTUAL\Admin_Projects\curr\data\log
+start C:\WORKS_2\WS\WS_Others.prog\prog\D-7\2_2\VIRTUAL\Admin_Projects\curr\data\csv_raw
+start C:\Users\iwabuchiken\AppData\Roaming\MetaQuotes\Terminal\34B08C83A5AAE27A4079DE708E60511E\MQL4
+
+exit
