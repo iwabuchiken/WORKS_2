7dbf545e3e88f7ba4f74de017732a95457c3f0b8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-03-09 13:03:11 +0900

    W (periodical updates : desktop PC) previous commit : `note PC` --> desktop

--- Utils/commands/push_WORKS.bat
+++ Utils/commands/push_WORKS.bat
@@ -6,6 +6,9 @@ echo cd C:\WORKS_2 -------------------
 
 REM C:\WORKS_2\a && cd C:\WORKS && git add . && git commit -m "W" && git push origin master
 REM C:\WORKS_2\a && cd C:\WORKS_2 && git add -A && git commit -m "W" && git push origin master
-C:\WORKS_2\a && cd C:\WORKS_2 && git add -A && git commit -m "W" && e && git push origin master
+rem C:\WORKS_2\a && cd C:\WORKS_2 && git add -A && git commit -m "W" && e && git push origin master
+
+rem : 2022”N3ŒŽ9“ú13:01:57 : modify message strings
+C:\WORKS_2\a && cd C:\WORKS_2 && git add -A && git commit -m "W (periodical updates : desktop PC)" && e && git push origin master
 	
 pause
